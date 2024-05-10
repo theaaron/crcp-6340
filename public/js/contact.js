59a30c056294667d4b7dc2053690bff96980a50e
aaron <4142467+theaaron@users.noreply.github.com>
2024-05-10 16:49:48 -0500

    styles and validation for contact form.

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,17 +1,38 @@
 (() => {
     "use strict";
 
+    const form = document.querySelector('#contact-form');
     let contactFormBtn = document.querySelector('#contactFormBtn');
+
+
+
     contactFormBtn.addEventListener('click', (event) => {
         event.preventDefault();
         event.stopPropagation();
+        
+        let isFormValid = true;
+        
+        if (!form.checkValidity()) {
+            isFormValid = false;
+        } 
+        form.classList.add('was-validated');
+        if (isFormValid) {
+            sendEmail();
+        }
+        
+    });
+    
+    
+    
+    function sendEmail() {
         let name = document.querySelector('#name').value;
         let mail = document.querySelector('#mail').value;
         let msg = document.querySelector('#msg').value;
         console.log(`name: ${name}`);
         console.log(`email: ${mail}`);
         console.log(`msg: ${msg}`);
-    });
+    }
+
 
 
 
