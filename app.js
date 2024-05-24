4a87db7dc8071691c148f1e898b7cc201fea55ed
aaron <4142467+theaaron@users.noreply.github.com>
2024-05-24 14:09:50 -0500

    installed ejs and adjusted for routing.

--- app.js
+++ app.js
@@ -6,13 +6,22 @@ dotenv.config();
 
 const app = express();
 const port = 3000;
-
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
 
-// app.get("/", (req, res) => {
-//     res.send("Hello World");
-// });
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+app.get("/projects", (req, res) => {
+    res.render("projects.ejs");
+});
+app.get("/project", (req, res) => {
+    res.render("project.ejs");
+});
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+});
 
 app.post("/mail", async (req, res) => {
     await utils
