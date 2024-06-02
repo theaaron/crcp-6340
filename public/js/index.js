4b13865827a841ce58f589ce8993c60d4a6b5736
aaron <4142467+theaaron@users.noreply.github.com>
2024-06-01 23:15:02 -0500

    added metamask functionality to connect button.

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,31 @@
 (() => {
     "use strict";
+    let userAddress = null;
+    let connect = document.querySelector('#about-contact-button');
+
+    connect.addEventListener('click', connectWallet());
+
+    async function connectWallet() {
+        await window.ethereum
+        .request({ method: "eth_requestAccounts"})
+        .then((data) => {
+            userAddress = data[0];
+            let walletString = userAddress.substring(0, 5) + "..." + userAddress.substring(38, 42);
+            connect.innerHTML = walletString;
+            return userAddress;
+        })
+        .catch((err) => {
+            if (err.code === 4001) {
+                // EIP-1193 userRejectedRequest error
+                // If this happens, the user rejected the connection request.
+                console.log('please connect a wallet');
+            } else {
+                console.log(err);
+            }
+        });
+    }
+
+
     let degreeRot = 6;
     let heroBg = document.getElementById('hero-title');
     function rotateHeroBg() {
