6db08ea8e781bb8ff6e86b43ab6c8edd8c89dd11
aaron <4142467+theaaron@users.noreply.github.com>
2024-06-18 16:33:49 -0500

    small cleanup before deployment,

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import * as utils from './utils/utils.js';
-import * as db from './utils/database.js';
+import express from "express";
+import dotenv from "dotenv";
+import * as utils from "./utils/utils.js";
+import * as db from "./utils/database.js";
 
 dotenv.config();
 
-const data = ['proj 1', 'proj 2', 'proj 3'];
+const data = ["proj 1", "proj 2", "proj 3"];
 let projects = [];
 let projects2 = [];
 
@@ -16,56 +16,54 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-    await db.connect()
-    .then(async() => {
-        projects = await db.getAllProjects();
-        console.log(projects);
-        projects.forEach((proj) => {proj.img_url = proj.img_url.replace("/public", "")});
-        let feat = getRandomInt(projects.length);
-        res.render("index.ejs", {projectArray: projects, featProj: feat});
-    })
+  await db.connect().then(async () => {
+    projects = await db.getAllProjects();
+    console.log(projects);
+    // projects.forEach((proj) => {
+    //   proj.img_url = proj.img_url.replace("/public", "");
+    // });
+    let feat = getRandomInt(projects.length);
+    res.render("index.ejs", { projectArray: projects, featProj: feat });
+  });
 });
 
 app.get("/projects", (req, res) => {
-        res.render("projects.ejs", {projectArray: projects});
+  res.render("projects.ejs", { projectArray: projects });
 });
 
 app.get("/project/:id", async (req, res) => {
-    let id = req.params.id;
-    if (id > data.length) {
-        throw new Error("No project with that ID")
-    }
-    await db.connect()
-    .then(async() => {
-        projects = await db.getAllProjects();
-        console.log(projects);
-        projects.forEach((proj) => {proj.img_url = proj.img_url.replace("/public", "")});
-        let feat = getRandomInt(projects.length);
-        res.render("project.ejs", {projectArray: projects, which: id});
-    })
-    
-
+  let id = req.params.id;
+  if (id > data.length) {
+    throw new Error("No project with that ID");
+  }
+  await db.connect().then(async () => {
+    projects = await db.getAllProjects();
+    console.log(projects);
+    // projects.forEach((proj) => {
+    //   proj.img_url = proj.img_url.replace("/public", "");
+    // });
+    res.render("project.ejs", { projectArray: projects, which: id });
+  });
 });
 app.get("/contact", (req, res) => {
-    res.render("contact.ejs");
+  res.render("contact.ejs");
 });
 
 app.post("/mail", async (req, res) => {
-    await utils
+  await utils
     .sendMsg(req.body.sub, req.body.txt)
     .then(() => {
-        res.send({result: "success"});
+      res.send({ result: "success" });
     })
     .catch(() => {
-        res.send({result: "failure"});
+      res.send({ result: "failure" });
     });
 });
 
 app.listen(port, () => {
-    console.log(`App running @ http://localhost:${port}`);
-})
-
+  console.log(`App running @ http://localhost:${port}`);
+});
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
+  return Math.floor(Math.random() * max);
+}
--- utils/database.js
+++ utils/database.js
@@ -1,28 +1,39 @@
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
+import mysql from "mysql2";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 let pool;
 
 export async function connect() {
-    let cString = "mysql://" + process.env.MYSQL_USER + ":" + process.env.MYSQL_PASSWORD + "@" + process.env.MYSQL_HOST + ":" + process.env.MYSQL_PORT + "/" + process.env.MYSQL_DATABASE;
-    console.log(cString);
+  let cString =
+    "mysql://" +
+    process.env.MYSQL_USER +
+    ":" +
+    process.env.MYSQL_PASSWORD +
+    "@" +
+    process.env.MYSQL_HOST +
+    ":" +
+    process.env.MYSQL_PORT +
+    "/" +
+    process.env.MYSQL_DATABASE;
+  console.log(cString);
 
-    pool = mysql.createPool(
-        //cString; //digital ocean server
-        {
-            host: process.env.MYSQL_HOST,
-            user: process.env.MYSQL_USER,
-            password: process.env.MYSQL_PASSWORD,
-            database: process.env.MYSQL_DATABASE,
-        }
+  pool = mysql
+    .createPool(
+      cString //digital ocean server
+      //   {
+      //     host: process.env.MYSQL_HOST,
+      //     user: process.env.MYSQL_USER,
+      //     password: process.env.MYSQL_PASSWORD,
+      //     database: process.env.MYSQL_DATABASE,
+      //   }
     )
     .promise();
 }
 
 export async function getAllProjects() {
-    const [rows] = await pool.query(`SELECT * FROM projects`);
-    console.log(rows);
-    return rows;
+  const [rows] = await pool.query(`SELECT * FROM projects`);
+  console.log(rows);
+  return rows;
 }
