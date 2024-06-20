89f9d5607eeab2d23353e88e5c6da76c59fbe4f0
aaron <4142467+theaaron@users.noreply.github.com>
2024-06-20 13:18:46 -0500

    rotateHeroBg only runs when necessary now.

--- public/js/index.js
+++ public/js/index.js
@@ -1,42 +1,41 @@
 (() => {
-    "use strict";
-    let userAddress = null;
-    let connect = document.querySelector('#connect-wallet-button');
+  "use strict";
+  let userAddress = null;
+  let connect = document.querySelector("#connect-wallet-button");
 
-    connect.addEventListener('click', connectWallet);
+  connect.addEventListener("click", connectWallet);
 
-    async function connectWallet() {
-        await window.ethereum
-        .request({ method: "eth_requestAccounts"})
-        .then((data) => {
-            userAddress = data[0];
-            let walletString = userAddress.substring(0, 5) + "..." + userAddress.substring(38, 42);
-            connect.innerHTML = walletString;
-            return userAddress;
-        })
-        .catch((err) => {
-            if (err.code === 4001) {
-                // EIP-1193 userRejectedRequest error
-                // If this happens, the user rejected the connection request.
-                console.log('please connect a wallet');
-            } else {
-                console.log(err);
-            }
-        });
-    }
+  async function connectWallet() {
+    await window.ethereum
+      .request({ method: "eth_requestAccounts" })
+      .then((data) => {
+        userAddress = data[0];
+        let walletString =
+          userAddress.substring(0, 5) + "..." + userAddress.substring(38, 42);
+        connect.innerHTML = walletString;
+        return userAddress;
+      })
+      .catch((err) => {
+        if (err.code === 4001) {
+          // EIP-1193 userRejectedRequest error
+          // If this happens, the user rejected the connection request.
+          console.log("please connect a wallet");
+        } else {
+          console.log(err);
+        }
+      });
+  }
 
+  let degreeRot = 6;
+  let heroBg = document.getElementById("hero-title");
 
-    let degreeRot = 6;
-    let heroBg = document.getElementById('hero-title');
-    function rotateHeroBg() {
-        heroBg.style.background = `repeating-linear-gradient(-${degreeRot}deg, rgb(108, 0, 118), rgb(41, 0, 88) 1%)`;
-        degreeRot += 0.5;
-        requestAnimationFrame(rotateHeroBg);
-    }
+  function rotateHeroBg() {
+    heroBg.style.background = `repeating-linear-gradient(-${degreeRot}deg, rgb(108, 0, 118), rgb(41, 0, 88) 1%)`;
+    degreeRot += 0.5;
+    requestAnimationFrame(rotateHeroBg);
+  }
 
+  if (heroBg) {
     rotateHeroBg();
-
-
-
-    
+  }
 })();
